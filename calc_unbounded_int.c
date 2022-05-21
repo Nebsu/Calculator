d08e0fafef13499a2677962ae86a92c5cb46b462
zeynali <k.p.zeynali@outlook.fr>
2022-05-21 18:01:17 +0200

    version pourrie des fonctions

--- calc_unbounded_int.c
+++ calc_unbounded_int.c
@@ -41,72 +41,156 @@ variables *get(variables *list, char* name){
     return NULL;
 }
 
-int isAffectation(const char *line) {
-    int egal = 0;
-    size_t n = strlen(line);
-    for (int i=0; i<n; i++) {
-        if (line[i] == '(' || line[i] == ')') return 0;
-        if (line[i] =='*' || line[i] =='+' || line[i] =='-') return 0;
-        if (line[i] =='=') egal += 1;
-    }
-    // Verification print :
+int containsPrint(const char *line, int i) {
     char *mot = malloc(5 * sizeof(char));
     if (mot == NULL) {perror("malloc"); return 0;} 
-    for (int i=0; i<n-5; i++) {
-        memmove(mot, line[i], sizeof(mot));
-        if (strcmp(mot, "print")==0) return 0;
-    }
-    // if variable non intialisée return 1 
-    if (egal > 1) return 0;
-    return 1;
+    memmove(mot, line[i], sizeof(mot));
+    if (strcmp(mot, "print")==0) return 1;
+    return 0;
+}
+
+int isSpecialChar(char c) { 
+    if (isalnum(c) == 0) return 1;
+    else return 0;
 }
 
 int isPrint(const char *line) {
     int print = 0;
     size_t n = strlen(line);
-    for (int i=0; i<n; i++) {
-        if (line[i] == '(' || line[i] == ')') return 0;
-        if (line[i]=='*' || line[i]=='+' || line[i]=='-') return 0;
-        if (line[i]=='=') return 0;
-    }
-    // Verification print :
-    char *mot = malloc(5 * sizeof(char));
-    if (mot == NULL) {perror("malloc"); return 0;} 
     for (int i=0; i<n-5; i++) {
-        memmove(mot, line[i], sizeof(mot));
-        if (strcmp(mot, "print")==0) return 1;
+        if (line[i] == ' ') continue;
+        if (containsPrint(line, i) == 1) {
+            print = i + 5;
+            break;
+        }
+        return 0;
     }
-    return 0;
+    if (print == 0) return 0;
+    for (int i=print; i<n; i++) {
+        if (line[i] == ' ') continue;
+        if (isalpha(line[i]) >= 1) break;
+        return 0;
+    }
+    return 1;
+}
+
+int isAffectation(const char *line) {
+    int egal = 0;
+    int var = 0;
+    size_t n = strlen(line);
+    for (int i=0; i<n; i++) {
+        // Cas espace :
+        if (line[i] == ' ') continue;
+        // Verif print :
+        if (containsPrint(line, i) == 1) return 0;
+        // Cas variable :
+        if (isalpha(line[i]) >= 1) {
+            if (var == 1) return 0;
+            var = 1;
+            for (int j=i+1; isalpha(line[j]) >= 1; j++) {i = j;}
+            continue;
+        }
+        // Cas égal :
+        if (line[i] == '=') {
+            if (var == 0) return 0;
+            egal = i + 1;
+            break;
+        }
+        return 0;
+    }
+    if (egal == 0 && var == 1) return 1;
+    if (egal == 0 && var == 0) return 0;
+    var = 0;
+    int val = 0;
+    for (int i=egal; i<n; i++) {
+        // Cas espace :
+        if (line[i] == ' ') continue;
+        // Verif print :
+        if (containsPrint(line, i) == 1) return 0;
+        // Cas variable :
+        if (isalpha(line[i]) == 1) {
+            if (val == 1) return 0;
+            if (var == 1) return 0;
+            var = 1;
+            for (int j=i+1; isalpha(line[j]) >= 1; j++) {i = j;}
+            continue;
+        }
+        // Cas valeur :
+        if (isdigit(line[i]) >= 1) {
+            if (var == 1) return 0;
+            if (val == 1) return 0;
+            val = 1;
+            for (int j=i+1; isdigit(line[j]) >= 1; j++) {i = j;}
+            continue;
+        }
+        return 0;
+    }
+    return 1;
 }
 
 int isOperation(const char *line) {
     int egal = 0;
-    int operation = 0;
+    int var = 0;
     size_t n = strlen(line);
-    for (int i=1; i<n-1; i++) {
-        if (line[i] == '(' || line[i] == ')') return 0;
-        // Egal :
-        if (line[i] =='=') egal +=1;
-        if (line[i-1] =='=') egal +=1;
-        if (line[i+1] =='=') egal +=1;
-        // Operation :
-        if (line[i]=='*' || line[i]=='+' || line[i]=='-') {
-            if (line[i+1] == ' ' && line[i-1] == ' ') operation += 1;
+    for (int i=0; i<n; i++) {
+        // Cas espace :
+        if (line[i] == ' ') continue;
+        // Verif print :
+        if (containsPrint(line, i) == 1) return 0;
+        // Cas variable :
+        if (isalpha(line[i]) == 1) {
+            if (var == 1) return 0;
+            var = 1;
+            for (int j=i+1; isalpha(line[j]) >= 1; j++) {i = j;}
+            continue;
         }
+        // Cas égal :
+        if (line[i] == '=') {
+            if (var == 0) return 0;
+            egal = i + 1;
+            break;
+        }
+        return 0;
     }
-    if (egal != 1 || operation != 1) return 0;
-    // Verification print :
-    char *mot = malloc(5 * sizeof(char));
-    if (mot == NULL) {perror("malloc"); return 0;} 
-    for (int i=0; i<n-5; i++) {
-        memmove(mot, line[i], sizeof(mot));
-        if (strcmp(mot, "print")==0) return 0;
+    if (egal == 0 && var == 1) return 1;
+    if (egal == 0 && var == 0) return 0;
+    var = 0;
+    int val = 0;
+    int op = 0;
+    for (int i=egal; i<n-1; i++) {
+        // Cas espace :
+        if (line[i] == ' ') continue;
+        // Verif print :
+        if (containsPrint(line, i) == 1) return 0;
+        // Cas variable :
+        if (isalpha(line[i]) == 1) {
+            if (val == 1) return 0;
+            if (var == 1) return 0;
+            var = 1;
+            for (int j=i+1; isalpha(line[j]) >= 1; j++) {i = j;}
+            continue;
+        }
+        // Cas valeur :
+        if (isdigit(line[i]) >= 1) {
+            if (var == 1) return 0;
+            if (val == 1) return 0;
+            val = 1;
+            for (int j=i+1; isdigit(line[j]) >= 1; j++) {i = j;}
+            continue;
+        }
+        // Cas opération :
+        if (line[i]=='*' || line[i]=='+' || line[i]=='-') {
+            if (op == 1) return 0;
+            op = 1;
+
+        }
+        return 0;
     }
     return 1;
 }
 
 void readFile(FILE *file) {
-    
+
 }
 
 int main(int argc, char const *argv[]){
