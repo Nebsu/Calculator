76a8026600ec31bbcad036de51766c5fc45e8c4e
zeynali <k.p.zeynali@outlook.fr>
2022-05-22 12:26:14 +0200

    Fonctions de détection

--- calc_unbounded_int.c
+++ calc_unbounded_int.c
@@ -1,35 +1,38 @@
-#include "unbounded_int.c"
 #include <stdio.h>
+#include <stdlib.h>
 #include <string.h>
 #include <assert.h>
-#include <stdlib.h>
+#include "unbounded_int.c"
 
-typedef struct mot mot;
-struct mot{
+// Structures :
+
+typedef struct Mot Mot;
+struct Mot {
     char* str;
-    struct mot *next;
+    struct Mot *next;
 };
- 
-typedef mot* Texte;
+typedef Mot* Texte;
 
-typedef struct variable {
+typedef struct Variable {
     char *label;
     unbounded_int value;
-    struct variable *next;
-} variable;
+    struct Variable *next;
+} Variable;
+typedef Variable* ListeVar;
 
-typedef variable* tableau;
+// Fonctions sur les variables :
 
-tableau addVar(tableau list, char* str, unbounded_int val){
-    variable* newElem = malloc(sizeof(variable));
+ListeVar addVar(ListeVar list, char* str, unbounded_int val) {
+    Variable* newElem = malloc(sizeof(Variable));
+    assert(newElem != NULL);
     newElem->label = str;
     newElem->next = NULL;
     newElem->value = val;
-    if(list == NULL){
+    if(list == NULL) {
         return newElem;
-    }else{
-        variable* temp=list;
-        while(temp->next != NULL){
+    } else {
+        Variable* temp=list;
+        while(temp->next != NULL) {
             temp = temp->next;
         }
         temp->next = newElem;
@@ -37,11 +40,11 @@ tableau addVar(tableau list, char* str, unbounded_int val){
     }
 }
 
-//Retourne la variable si elle est dans la liste, sinon retourne NULL
-variable *contains(variable *list, char* str){
-    variable *current = list;
+// Retourne la Variable si elle est dans la liste, sinon retourne NULL
+Variable *contains(Variable *list, char* str) {
+    Variable *current = list;
     while (current->next != NULL) {
-        if(strcmp(str, current -> label) == 0){
+        if(strcmp(str, current -> label) == 0) {
             return current;
         }
         current = current->next;
@@ -49,28 +52,129 @@ variable *contains(variable *list, char* str){
     return NULL;
 }
 
-void afficherTab(tableau liste){
-    variable *tmp = liste;
+void afficherListe(ListeVar liste) {
+    Variable *tmp = liste;
     while(tmp != NULL){
         printf("%s", tmp->label);
         tmp = tmp->next;
     }
 }
 
-int isAffectation(const char *line) {
+Texte ajouterFin(Texte liste, char* valeur){
+    Mot* newElem = malloc(sizeof(Mot));
+    newElem->str = valeur;
+    newElem->next = NULL;
+    if(liste == NULL){
+        return newElem;
+    }else{
+        Mot* temp=liste;
+        while(temp->next != NULL){
+            temp = temp->next;
+        }
+        temp->next = newElem;
+        return liste;
+    }
+}
+
+void afficherListe(Texte liste){
+    Mot *tmp = liste;
+    while(tmp != NULL){
+        printf("%s", tmp->str);
+        tmp = tmp->next;
+    }
+}
+
+void abc(char *str, ListeVar t){
+    t = addVar(t, str, ll2unbounded_int(1));
+}
+
+int isNumber(const char *str) {
+    size_t n = strlen(str);
+    for (int i=0; i<n; i++) {
+        if (isdigit(str[i]) == 0) return 0;
+    }
+    return 1;
+}
+
+int isWord(const char *str) {
+    size_t n = strlen(str);
+    for (int i=0; i<n; i++) {
+        if (isalpha(str[i]) == 0) return 0;
+    }
+    return 1;
+}
+
+// Fonctions qui détecte le type de la ligne (print, affectation ou opération) : 
+
+int isPrint(Texte ligne, Variable *variables) {
+    if (ligne == NULL) return 0;
+    afficherListe(ligne);
+    char *s1 = ligne->str;
+    if (isWord(s1) == 0) return 0;
+    if (strcmp(s1, "print") != 0) return 0;
+    ligne = ligne->next;
+    if (ligne == NULL) return 0;
+    char *s2 = ligne->str;
+    if (contains(variables, s2) == NULL) return 0;
+    return 1;
+}
+
+int isAffectation(Texte ligne, Variable *variables) {
+    if (ligne == NULL) return 0;
+    afficherListe(ligne);
+    char *s1 = ligne->str;
+    if (isWord(s1) == 0) return 0;
+    ligne = ligne->next;
+    // Cas variable non-initialisée :
+    if (ligne == NULL) return 1;
+    char *s2 = ligne->str;
+    if (strcmp(s2, "=") != 0) return 0;
+    ligne = ligne->next;
+    if (ligne == NULL) return 0;
+    char *s3 = ligne->str;
+    if (isNumber(s3) == 1) return 1;
+    if (isWord(s3) == 1) {
+        if (contains(variables, s3) != NULL) return 1;
+    }
     return 0;
 }
 
-int isPrint(const char *line) {
-    return 0;
+int isOperation(Texte ligne, Variable *variables) {
+    if (ligne == NULL) return 0;
+    afficherListe(ligne);
+    char *s1 = ligne->str;
+    if (isWord(s1) == 0) return 0;
+    if (contains(variables, s1) == NULL) return 0;
+    ligne = ligne->next;
+    if (ligne == NULL) return 0;
+    char *s2 = ligne->str;
+    if (strcmp(s2, "=") != 0) return 0;
+    ligne = ligne->next;
+    if (ligne == NULL) return 0;
+    char *s3 = ligne->str;
+    // On doit avoir des nombres ou des variables :
+    if (isNumber(s3) == 0 && isWord(s3) == 0) return 0;
+    if (isWord(s3) == 1) {
+        if (contains(variables, s3) == NULL) return 0;
+    }
+    ligne = ligne -> next;
+    if (ligne == NULL) return 0;
+    char *s4 = ligne->str;
+    if (strcmp(s4, "+") != 0 && strcmp(s4, "-") != 0 && strcmp(s4, "*") != 0) return 0;
+    ligne = ligne->next;
+    if (ligne == NULL) return 0;
+    char *s5 = ligne->str;
+    if (isNumber(s3) == 0 && isWord(s3) == 0) return 0;
+    if (isWord(s3) == 1) {
+        if (contains(variables, s3) == NULL) return 0;
+    }
+    return 1;
 }
 
-int isOperation(const char *line) {
-    return 0;
-}
+// Fonctions qui effectue les procédures du fichier texte (print, affectation ou opération) :
 
-void printVar(FILE* dest, Texte ligne, variable *list){
-    variable *v = contains(list, ligne -> str);
+void printVar(FILE* dest, Texte ligne, Variable *list) {
+    Variable *v = contains(list, ligne -> str);
     if(v != NULL){
         fprintf(dest, "%s = %s\n" , v -> label, unbounded_int2string(v -> value));
     }else{
@@ -78,31 +182,31 @@ void printVar(FILE* dest, Texte ligne, variable *list){
     }
 }
 
-void affectVar(Texte ligne, variable *list){
-    // Verifie si la variable est déja définie
-    variable *v1 = contains(list, ligne -> str);
+void affectVar(Texte ligne, Variable *list){
+    // Verifie si la Variable est déja définie
+    Variable *v1 = contains(list, ligne -> str);
     // On récupère l'affectation
-    mot *tmp = ligne;
+    Mot *tmp = ligne;
     while(strcmp(tmp -> str, "=") != 0){
         tmp = tmp -> next;
     }
     char *res = tmp -> next -> str;
     unbounded_int n;
-    // On vérifie si l'affectation est un entier ou une variable
+    // On vérifie si l'affectation est un entier ou une Variable
     // Si c'est un entier on transforme l'entier en u_int
-    // Sinon on récupère la variable et on vérifie si la variable est définie
+    // Sinon on récupère la Variable et on vérifie si la Variable est définie
     if(is_int(res) == 1){
         n = string2unbounded_int(res);
     }else{
-        variable *v2 = contains(list, res);
+        Variable *v2 = contains(list, res);
         if(v2 != NULL){
             n = v2 -> value;
         }else{
             n = init_result(1);
         }
     }
-    // Si la variable n'est pas définie on l'ajoute dans la liste avec l'affectation
-    // Sinon on modifie la valeur de la variable
+    // Si la Variable n'est pas définie on l'ajoute dans la liste avec l'affectation
+    // Sinon on modifie la valeur de la Variable
     if(v1 == NULL){
         list = addVar(list, ligne -> str, n);
     }else{
@@ -110,25 +214,25 @@ void affectVar(Texte ligne, variable *list){
     }
 }
 
-void OpVar(Texte ligne, variable *list, char signe){
-    // Verifie si la variable est déja définie
-    variable *v1 = contains(list, ligne -> str);
+void opVar(Texte ligne, Variable *list, char signe){
+    // Verifie si la Variable est déja définie
+    Variable *v1 = contains(list, ligne -> str);
     // On récupère l'affectation
-    mot *tmp = ligne;
+    Mot *tmp = ligne;
     while(strcmp(tmp -> str, "=") != 0){
         tmp = tmp -> next;
     }
     tmp = tmp -> next;
     char *res1 = tmp -> str;
     // Variable 1 :
-    // On vérifie si l'affectation est un entier ou une variable
+    // On vérifie si l'affectation est un entier ou une Variable
     // Si c'est un entier on transforme l'entier en u_int
-    // Sinon on récupère la variable et on vérifie si la variable est définie
+    // Sinon on récupère la Variable et on vérifie si la Variable est définie
     unbounded_int n1;
     if(is_int(res1) == 1){
         n1 = string2unbounded_int(res1);
     }else{
-        variable *v2 = contains(list, res1);
+        Variable *v2 = contains(list, res1);
         if(v2 != NULL){
             n1 = v2 -> value;
         }else{
@@ -142,7 +246,7 @@ void OpVar(Texte ligne, variable *list, char signe){
     if(is_int(res2) == 1){
         n2 = string2unbounded_int(res2);
     }else{
-        variable *v2 = contains(list, res2);
+        Variable *v2 = contains(list, res2);
         if(v2 != NULL){
             n2 = v2 -> value;
         }else{
@@ -169,73 +273,48 @@ void OpVar(Texte ligne, variable *list, char signe){
         }
 
     }
-
 }
 
-void readFile(FILE *file, variable *list){
-    Texte liste = NULL;
-    FILE *file = fopen("dest.txt", "r");
+void readFile(FILE *input, FILE *output) {
+    Texte texte = NULL;
+    ListeVar variables = malloc(sizeof(ListeVar));
+    assert(variables != NULL);
     int i;
-    //Ajoute toutes les lignes dans le liste
-    while((i = fgetc(file))!= EOF) {
-        if(i == '\n')liste = ajouterFin(liste, "\n");
-        char *mot = malloc(sizeof(char));
-        fscanf(file, "%s", mot);
-        liste = ajouterFin(liste, mot);
+    // Ajoute toutes les lignes dans le texte
+    while((i = fgetc(input))!= EOF) {
+        if( i == '\n') texte = ajouterFin(texte, "\n");
+        char *Mot = malloc(sizeof(char));
+        assert(Mot != NULL);
+        fscanf(input, "%s", Mot);
+        texte = ajouterFin(texte, Mot);
     }
-    Texte liste2;
-    //Recupere une ligne
-    while(strcmp(liste -> str, "\n") != 0){
-        liste2 = NULL;
-        liste2 = ajouterFin(liste2, liste -> str);
-        liste = liste -> next;
-    }
-    liste = liste -> next;
-    // Liste2 = Ligne 1
-    // Quand on refait la boucle on arrive sur la ligne 2
-    afficherListe(liste2);
-    while(strcmp(liste -> str, "\n") != 0){
-        liste2 = NULL;
-        liste2 = ajouterFin(liste2, liste -> str);
-        liste = liste -> next;
-    }
-    liste = liste -> next;
-    // afficherListe(liste2);
-    // afficherListe(liste);
-}
-
-Texte ajouterFin(Texte liste, char* valeur){
-    mot* newElem = malloc(sizeof(mot));
-    newElem->str = valeur;
-    newElem->next = NULL;
-    if(liste == NULL){
-        return newElem;
-    }else{
-        mot* temp=liste;
-        while(temp->next != NULL){
-            temp = temp->next;
+    Texte ligne;
+    // Récupère les lignes
+    while (texte != NULL) {
+        while(strcmp(texte -> str, "\n") != 0){
+            ligne = NULL;
+            ligne = ajouterFin(ligne, texte -> str);
+            texte = texte -> next;
         }
-        temp->next = newElem;
-        return liste;
-    }
-}
+        // Traitement de la ligne :
+        if (isPrint(ligne, variables) == 1) {
 
-void afficherListe(Texte liste){
-    mot *tmp = liste;
-    while(tmp != NULL){
-        printf("%s", tmp->str);
-        tmp = tmp->next;
-    }
-}
+        } else if (isAffectation(ligne, variables) == 1) {
 
-void abc(char *str, tableau t){
-    t = addVar(t, str, ll2unbounded_int(1));
+        } else if (isOperation(ligne, variables) == 1) {
+
+        } else {
+            fprintf(output, "Ligne vide ou invalide\n");
+        }
+        texte = texte -> next;
+        ligne = ligne -> next;
+    }
+    // Quand on refait la boucle on arrive sur la ligne 2
+    afficherListe(ligne);
+    // afficherListe(texte);
 }
 
 int main(int argc, char const *argv[]) {
-
-
-
     FILE* file1;
     FILE* file2;
     if (argc == 1) {
@@ -258,6 +337,6 @@ int main(int argc, char const *argv[]) {
         perror("Argument error");
         return 1;
     }
-
+    readfile(file1, file2);
     return 0;
 }
